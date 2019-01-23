a2ee6d47d73609c895bb94324563f8b64fbb1b40
julieyil <gj.yildirim@gmail.com>
2019-01-23 17:00:56 +0100

    servlet creer compte + creer compte jsp

--- src/org/trocencheres/servlets/ServletCreerCompte.java
+++ src/org/trocencheres/servlets/ServletCreerCompte.java
@@ -23,55 +23,68 @@ import org.trocencheres.dal.UtilisateurDAOFactory;
 public class ServletCreerCompte extends HttpServlet implements Servlet {
 	private static final long serialVersionUID = 1L;
 	private UtilisateurDAO daoUtilisateur;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletCreerCompte() {
-        super();
-        this.daoUtilisateur=UtilisateurDAOFactory.getUtilisateurDao();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request,response);
+	public ServletCreerCompte() {
+		super();
+		this.daoUtilisateur = UtilisateurDAOFactory.getUtilisateurDao();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//int credit;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// int credit;
 		String pseudo = request.getParameter("pseudo");
-		String nom =request.getParameter("nom");
-		String prenom=request.getParameter("prenom"); 
-		String email=request.getParameter("email");
-		String telephone=request.getParameter("telephone"); 
-		String rue=request.getParameter("rue");
-		String codePostal=request.getParameter("codepostal"); 
-		String ville=request.getParameter("ville");
-		String motDePasse=request.getParameter("motdepasse");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codepostal");
+		String ville = request.getParameter("ville");
+		String motDePasse = request.getParameter("motdepasse");
 		String confirmationMotDePasse = request.getParameter("confirmation");
-		
+
 		ArrayList<Integer> ventes = new ArrayList<>();
-		boolean administrateur=false;
-		int credit=0;
-		int noUtilisateur=0;
+		boolean administrateur = false;
+		int credit = 0;
+		int noUtilisateur = 0;
 		
+		Utilisateur utilisateurCree=new Utilisateur(noUtilisateur, pseudo, nom, prenom, email, telephone, rue,
+				codePostal, ville, credit, administrateur, ventes, motDePasse);
+
 		if (!motDePasse.equals(confirmationMotDePasse)) {
-					
-			this.getServletContext().getRequestDispatcher("/WEB-INF/creerCompte.jsp").forward(request, response);
-		}else {
+
+			this.getServletContext().getRequestDispatcher("/creerCompte.jsp").forward(request, response);
+		} else {
 			try {
-				daoUtilisateur.insert(new Utilisateur());
-			}catch (DALException e) {
+				//select by email pseudo et tel pour éviter double enregistrement pour une mm personne
+				daoUtilisateur.insert(utilisateurCree);
+				if(utilisateurCree.getNoUtilisateur()!=0) {
+					request.getSession().setAttribute("identifiant",utilisateurCree.getPseudo());
+					this.getServletContext().getRequestDispatcher("/WEB-INF/monProfil.jsp").forward(request, response);
+				}
+			} catch (DALException e) {
 				request.setAttribute("erreur", e);
 				this.getServletContext().getRequestDispatcher("/WEB-INF/erreur.jsp").forward(request, response);
-				
+
 			}
 		}
-		
+
 	}
 
 }
